20aa4049e9dc7b357a355febf0d0be5fe85b9a49
khaoula <bakasskhaoula9@gmail.com>
2023-06-05 17:35:46 -0700

    Linked list cycle

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
